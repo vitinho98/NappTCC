bc1765a507942ccbf98d3d760c44e5deb78a3462
vitinho98 <42591751+vitinho98@users.noreply.github.com>
2019-03-21 15:41:24 -0300

    commit inicial

---
+++ app/src/main/java/com/fatecourinhos/napp/AlunoActivity.java
@@ -0,0 +1,14 @@
+package com.fatecourinhos.napp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno);
+    }
+}
