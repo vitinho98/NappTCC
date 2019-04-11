28be60db9bd4715c9d4273339c8effb50e5c5927
irsocafuso <wilsinho_sanches@hotmail.com>
2019-04-11 17:12:45 -0300

    campoAtuacaoModel

--- app/src/main/java/com/fatecourinhos/napp/model/CampoAtuacaoModel.java
+++ app/src/main/java/com/fatecourinhos/napp/model/CampoAtuacaoModel.java
@@ -1,4 +1,31 @@
 package com.fatecourinhos.napp.model;
 
 public class CampoAtuacaoModel {
+    private int idCampoAtuacao;
+    private String nomeCampoAtuacao;
+
+    CampoAtuacaoModel(){
+
+    }
+
+    CampoAtuacaoModel(int idCampoAtuacao, String nomeCampoAtuacao){
+        this.idCampoAtuacao = idCampoAtuacao;
+        this.nomeCampoAtuacao = nomeCampoAtuacao;
+    }
+
+    public int getIdCampoAtuacao() {
+        return idCampoAtuacao;
+    }
+
+    public void setIdCampoAtuacao(int idCampoAtuacao) {
+        this.idCampoAtuacao = idCampoAtuacao;
+    }
+
+    public String getNomeCampoAtuacao() {
+        return nomeCampoAtuacao;
+    }
+
+    public void setNomeCampoAtuacao(String nomeCampoAtuacao) {
+        this.nomeCampoAtuacao = nomeCampoAtuacao;
+    }
 }
