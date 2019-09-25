501ba53a7f0597dbe030536fa03d4dfa835269d1
vitinho98 <42591751+vitinho98@users.noreply.github.com>
2019-09-25 14:46:50 -0300

    .

--- app/src/main/java/com/fatecourinhos/napp/view/cadastros/CadastroAgendamento.java
+++ app/src/main/java/com/fatecourinhos/napp/view/cadastros/CadastroAgendamento.java
@@ -1,29 +1,40 @@
 package com.fatecourinhos.napp.view.cadastros;
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fatecourinhos.napp.R;
+import com.fatecourinhos.napp.json.HorarioProfissionalJSONParser;
 import com.fatecourinhos.napp.json.ProfissionalJSONParser;
 import com.fatecourinhos.napp.model.CampoAtuacao;
+import com.fatecourinhos.napp.model.Horario;
 import com.fatecourinhos.napp.model.Profissional;
 import com.fatecourinhos.napp.util.HttpManager;
 import com.fatecourinhos.napp.util.RequestHttp;
+import com.fatecourinhos.napp.view.adapter.HorarioProfissionalAdapter;
 import com.fatecourinhos.napp.view.adapter.ProfissionalAdapter;
 import com.fatecourinhos.napp.view.fragments.ProfissionalFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import sun.bob.mcalendarview.MCalendarView;
+import sun.bob.mcalendarview.views.ExpCalendarView;
+
 public class CadastroAgendamento extends AppCompatActivity {
 
+    ExpCalendarView calendarView;
+    List<Horario> agendaProfissional;
     Spinner spinner;
     String conteudo;
     List<Profissional> profissionais = new ArrayList<>();
@@ -33,11 +44,14 @@ public class CadastroAgendamento extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agendamento_aluno);
+        calendarView = findViewById(R.id.calendario);
         spinner = findViewById(R.id.spinner_profissional);
         selecionarProfissionais();
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                selecionarDatas(profissionais.get(spinner.getSelectedItemPosition()).getIdProfissional());
 
             }
 
@@ -59,15 +73,17 @@ public class CadastroAgendamento extends AppCompatActivity {
         requestHttp.setParametro("id", String.valueOf(id));
         mytask.execute(requestHttp);
     }
-    private class SelecionarDatas extends AsyncTask<RequestHttp, String, String> {
+
+    private class SelecionarDatas extends AsyncTask<RequestHttp, String, List<Horario>> {
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
-        protected String doInBackground(RequestHttp... params) {
+        protected List<Horario> doInBackground(RequestHttp... params) {
 
             try {
                 conteudo = HttpManager.getDados(params[0]);
@@ -75,13 +91,13 @@ public class CadastroAgendamento extends AppCompatActivity {
                 conteudo = null;
             }
 
-            return conteudo;
+            agendaProfissional = HorarioProfissionalJSONParser.parseDados(conteudo);
+            return agendaProfissional;
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
+        protected void onPostExecute(final List<Horario> agendasProfissional) {
+            super.onPostExecute(agendasProfissional);
         }
 
     }
